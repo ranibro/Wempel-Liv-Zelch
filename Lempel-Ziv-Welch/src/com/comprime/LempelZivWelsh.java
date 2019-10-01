99a3ed462993d92f5985453d091f7d12e69379ce
ranibro <44777828+ranibro@users.noreply.github.com>
2019-09-30 19:46:13 -0500

    Add files via upload

--- Lempel-Ziv-Welch/src/com/comprime/LempelZivWelsh.java
+++ Lempel-Ziv-Welch/src/com/comprime/LempelZivWelsh.java
@@ -1,36 +1,65 @@
 package com.comprime;
 
 import java.util.*;
+import java.io.*;
 
 public class LempelZivWelsh {
-	public static List<Integer> encode(String texto) {
-		// Construindo o dicionário de indices.
+	@SuppressWarnings("resource")
+	public static void encode(File arq, String dir) throws IOException {
 
+		// Direct = Diretório do Window OU Linux, user.home pega o atual usuário e seu
+		// diretório
+		File direct = new File(System.getProperty("user.home") + "/Desktop");
+
+		// Arquivo de saída
+		File saida = new File(direct, dir);
+		InputStream is = new FileInputStream(arq);
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		// String linha vai receber a próxima linha do arquivo parâmetro pelo
+		// BufferedReader;
+		String linha = br.readLine();
+
+		// Texto vai ser a concatenação das linhas.
+		String texto = linha;
+		while (linha != null) {
+			linha = br.readLine();
+			texto = texto + linha;
+
+		}
+		br.close();
+
+		// Construindo o dicionário de indices;
 		int tamanho = 128;
 		Map<String, Integer> dicionario = new HashMap<String, Integer>();
+
 		// Criar uma Lista de Vetores HashMap com os indices do dicionário, inicialmente
 		// vai ser limpo, a cada etapa do i
 		// vai copiando os indices do HashMap nesse dicionário, que são os ASCII;
-
 		for (int i = 0; i < tamanho; i++) {
-			// indice i que vai receber o caractere ASCII.
 
+			// indice i que vai receber o caractere ASCII.
 			dicionario.put("" + (char) i, i);
 		}
 
 		String palavra = "";
+
 		// Lista de Vetores
 		List<Integer> resultado = new ArrayList<Integer>();
 		List<Integer> vetTemp = new ArrayList<Integer>();
+
 		// Converte o texto em vetor de caracteres, palavra = caracteres entre espaços
 		// vazios
-		// o loop recebera valor null ao final dela e entrará em condição de parada;
-
+		// o loop recebera valor null ao final dela e entrará em condição de parada
 		for (char proxCaractere : texto.toCharArray()) {
+
 			// Pega o prox caractere
 			String ab = "" + proxCaractere;
+
 			// Procura o espaço, se não houver, ele concatena o caractere à palavra;
 			if (!(proxCaractere == ' ')) {
+
 				// Procura se ja existe a palavra no dicionário, caso exista, ele pega o indice
 				// e adiciona
 				// no vetor temporário
@@ -40,6 +69,7 @@ public class LempelZivWelsh {
 				// Concatena toda a palavra com o proximo caractere dela, se não for espaço.
 				palavra = palavra + ab;
 			} else {
+
 				// Se houver espaço, finaliza palavra, e ve se ela existe no dicionário, se
 				// não ele cria um novo indice
 				if (dicionario.containsKey(palavra)) {
@@ -59,42 +89,137 @@ public class LempelZivWelsh {
 
 		if (!palavra.equals(""))
 			resultado.add(dicionario.get(palavra));
-		return resultado;
+
+		try {
+			saida.createNewFile();
+			FileWriter escr = new FileWriter(saida);
+			escr.write(resultado.toString());
+		} catch (IOException e) {
+			System.out.println("Erro ao criar arquivo");
+			e.printStackTrace();
+		}
 	}
 
-	public static String decode(List<Integer> arquivo) {
+	public static void decode(File arq, String out) throws IOException { // List<Integer> arquivo
+		File direct = new File(System.getProperty("user.home") + "/Desktop");
+		File saida = new File(direct, out);
+		List<Integer> arquivo = new ArrayList<Integer>();
+		List<String> temp = new ArrayList<String>();
+		InputStream is = new FileInputStream(arq);
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+
+		br.read();
+		String texto = "";
+		String caracter = "";
+		int valor;
+		while ((valor = br.read()) != -1) {
+			caracter = String.valueOf((char) valor);
+			if (caracter == "]") {
+				break;
+			} else {
+				texto = texto + caracter;
+			}
+		}
+		br.close();
+
+		// Como o arquivo bin que vai receber vai ter vários indices, separamos eles por
+		// virgulas, como num arquivo CSV
+		temp = Arrays.asList(texto.split(","));
+		for (String str : temp) {
+			StringBuilder sb = new StringBuilder(str);
+			if (str.charAt(0) == ' ')
+				sb.deleteCharAt(0);
+			str = sb.toString();
+			arquivo.add(Integer.parseInt(str));
+		}
+
 		// Monta o dicionário padrão ASCII
 		int tamanho = 128;
 		Map<Integer, String> dicionario = new HashMap<Integer, String>();
 		for (int i = 0; i < tamanho; i++)
 			dicionario.put(i, "" + (char) i);
 
-		// Cria variável que vai recever o indice do primeiro cacacter do arquivo
-		// comprimido
 		String ab = "";
+
+		// Stringbuffer que vai receber todo o texto;
 		StringBuffer resultado = new StringBuffer(ab);
 		String palavra = "";
+
 		// Loop recebe os indices o arquivo até ficar vazio
 		for (int indice : arquivo) {
-			if(indice == 32) {
-				dicionario.put(tamanho++, palavra); 
+
+			// Quando o indice encontrar espaço, ele encerra palavra e coloca ela no
+			// dicionário e no resultado
+			if (indice == 32) {
+				dicionario.put(tamanho++, palavra);
 				resultado.append(palavra);
 				resultado.append(" ");
 				palavra = "";
-			}
-			else
-				if (dicionario.containsKey(indice))
-					palavra = palavra + dicionario.get(indice);
+			} else
+
+			// Quandno não encontra, ele concatena a palavra
+			if (dicionario.containsKey(indice))
+				palavra = palavra + dicionario.get(indice);
 
 		}
 		resultado.append(palavra);
-		return resultado.toString();
+
+		try {
+			saida.createNewFile();
+			FileWriter escr = new FileWriter(saida);
+			escr.write(resultado.toString());
+			escr.close();
+		} catch (IOException e) {
+			System.out.println("Erro ao criar arquivo");
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		List<Integer> compressed = encode("This is as Simple as it is");
-		System.out.println(compressed);
-		String decompressed = decode(compressed);
-		System.out.println(decompressed);
+		Scanner scan = new Scanner(System.in);
+		String in = "", out = "";
+		System.out.println("Insira o código");
+		String[] comando = new String[5];
+		for (int i = 0; i < 5; i++) {
+			comando[i] = scan.next();
+		}
+		scan.close();
+
+		in = comando[2];
+		out = comando[4];
+		
+		/*if (comando[0].equals("encode")) {
+			in = comando[2];
+			out = comando [4];
+		}else if(comando[0].equals("decode")){
+			in = comando[2];
+			out = comando[4];
+		}else 
+			System.out.println("Comando inserido inválido");
+			*/
+		
+		File arq = new File(System.getProperty("user.home") + "/Desktop/" + in);
+		
+		// O nome do arquivo que o 'encode' vái gerar é sai.bin, então tem que servir de
+		// entrada para o decode
+		File arq2 = new File(System.getProperty("user.home") + "/Desktop/" + in);
+		try {
+			if(comando[0].equals("encode")) 
+				encode(arq, out);
+			else if(comando[0].equals("decode")) 
+					decode(arq2, out);
+				else
+					System.out.println("Comando inserido inválido"); 
+			
+		} catch (IOException e) {
+			System.out.println("Arquivo não existente");
+			e.printStackTrace();
+		}
+
+		// System.out.println(resultado);
+		// System.out.println(compressed);
+		// String decompressed = decode(compressed);
+		// System.out.println(decompressed);
 	}
 }
